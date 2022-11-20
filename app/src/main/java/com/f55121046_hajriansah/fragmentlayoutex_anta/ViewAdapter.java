5702faace77b1993a96b82f90dfc1832714cbf9d
antha <ahajriansah@gmail.com>
2022-11-20 20:51:45 +0800

    Initial commit

---
+++ app/src/main/java/com/f55121046_hajriansah/fragmentlayoutex_anta/ViewAdapter.java
@@ -0,0 +1,7 @@
+package com.f55121046_hajriansah.fragmentlayoutex_anta;
+
+import androidx.fragment.app.Fragment;
+
+public interface ViewAdapter {
+    void AddFragment(Fragment fragment, String Title);
+}
